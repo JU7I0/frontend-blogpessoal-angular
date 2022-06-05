fac6f0463c241f25e14d9833200fc8ea9a6595b9
Julio Barros <juliocezarbarros2@gmail.com>
2022-06-05 13:06:47 -0300

    Implementando funções de cadastrar e cancelar

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+    usuario: Usuario = new Usuario //variáveis acima dos costrutores
+    confirmarSenha: string
+    tipoUsuario: string
+  
+    //injeção de dependência. O módulo cadastrar DEPENDE do AuthService
+    constructor(
+      private authService: AuthService, 
+      private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
   }
 
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoDoUsuario(event: any) {
+    this.tipoUsuario = event.target.value
+
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoUsuario
+    if(this.usuario.senha != this.confirmarSenha){
+      alert("Atenção! As senhas não coincidem.")
+  } else { 
+    this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+      this.usuario = resp
+      this.router.navigate(["/entrar"])
+      alert("Usuário cadastrado com sucesso!")
+    }) //sobrescreve o objeto para formato JSON
+  }
+}
 }
