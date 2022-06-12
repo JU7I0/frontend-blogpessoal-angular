17071554f65735419a4c9dbb7ee98dd472630614
Julio Barros <juliocezarbarros2@gmail.com>
2022-06-12 03:56:36 -0300

    Implemtentando rotas e modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemasComponent } from './temas/temas.component';
 
 const routes: Routes = [
 
-  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
   {path: 'inicio', component: InicioComponent},
   {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'temas', component: TemasComponent},
+  {path: 'temaEdit/:id', component: TemaEditComponent},
+  {path: 'temaDelete/:id', component: TemaDeleteComponent},
+  {path: 'postagemEdit/:id', component: PostagemEditComponent},
+  {path: 'postagemDelete/:id', component: PostagemDeleteComponent}
+
 ];
 
 @NgModule({
